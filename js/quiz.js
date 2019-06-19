8693bc5991edd07d2def4a3e19bae5ce037be7b4
zhongchai <z.chai@students.clark.edu>
2019-06-19 14:44:52 -0700

    completed assignment

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     var questionCount = 0;
     var numCorrect = 0;
     var userScore = 0;
     var difficulty = "";
     var currentQuestion = [];
-    
-    // called when the New Question button is clicked
-    $('#new').click(function() {
 
+    // hides the main quiz module upon load
+    $('#quiz').hide();
+
+    // audio controls
+    $(".my_audio").trigger('load');
+
+    var play_audio = function (task) {
+        if (task == 'play') {
+            $(".my_audio").trigger('play');
+        }
+        if (task == 'stop') {
+            $(".my_audio").trigger('pause');
+            $(".my_audio").prop("currentTime", 0);
+        }
+    }
+
+    // handles getting the question from the api and displaying it
+    var runQuiz = function () {
         questionCount++;
 
         // first, reset the page values
@@ -23,7 +38,7 @@ $(document).ready(function() {
         // start api call
         $.ajax({
             url: "http://jservice.io/api/random"
-        }).then(function(data) {
+        }).then(function (data) {
             // get some data from the api and push it into an array
             currentQuestion.push(data[0].value);
             currentQuestion.push(data[0].category.title);
@@ -37,48 +52,67 @@ $(document).ready(function() {
             $('#category').html(currentQuestion[1].charAt(0).toUpperCase() + currentQuestion[1].slice(1));
             $('#difficulty').html(difficulty);
             $('#content').html(currentQuestion[2]);
-
         });
+    }
+
+    // called when the Start Quiz button is called
+    // hides/shows elements, then runs the first question in the quiz
+    $('#start').click(function () {
+        runQuiz();
+        $('#quiz-start').hide();
+        $('#quiz').show();
+
+
     });
 
-  $('#submit').click(function() {
-    var inputAnswer = $("#useranswer").val();
-    if (inputAnswer.trim().toLowerCase() === currentQuestion[3].trim().toLowerCase()) {
-        numCorrect++;
-        userScore += currentQuestion[0];
-        $('#feedback').html("Correct!");
-    } else {
-        $('#feedback').html("Sorry, the correct answer is: " + currentQuestion[3]);
-    }
-  });
+    // called when the New Question button is clicked
+    $('#new').click(runQuiz);
 
-  // function to turn the Jeopardy category difficulty numbers into readable words
-  var parseDifficulty = function(key) {
-    switch (key) {
-        case 100:
-        case 200:
-            difficulty = "Very Easy";
-            break;
-        case 300:
-        case 400:
-            difficulty = "Medium";
-            break;
-        case 500:
-        case 600:
-            difficulty = "Hard";
-            break;
-        case 700:
-        case 800:
-            difficulty = "Very Hard";
-            break;
-        case 900:
-        case 1000:
-            difficulty = "Super Hard";
-            break;
-        default:
-            difficulty = "Unknown";
-            break;
+    // called when the End Quiz button is clicked
+    $('#end').click(function () {
+        localStorage.setItem('questionCount', questionCount);
+        localStorage.setItem('numCorrect', numCorrect);
+    });
+
+    // handles submission and checking of answer input
+    $('#submit').click(function () {
+        var inputAnswer = $("#useranswer").val();
+        if (inputAnswer.trim().toLowerCase() === currentQuestion[3].trim().toLowerCase()) {
+            numCorrect++;
+            userScore += currentQuestion[0];
+            $('#feedback').html("Correct!");
+        } else {
+            $('#feedback').html("Sorry, the correct answer is: " + currentQuestion[3]);
+        }
+    });
+
+    // function to turn the Jeopardy category difficulty numbers into readable words
+    var parseDifficulty = function (key) {
+        switch (key) {
+            case 100:
+            case 200:
+                difficulty = "Very Easy";
+                break;
+            case 300:
+            case 400:
+                difficulty = "Medium";
+                break;
+            case 500:
+            case 600:
+                difficulty = "Hard";
+                break;
+            case 700:
+            case 800:
+                difficulty = "Very Hard";
+                break;
+            case 900:
+            case 1000:
+                difficulty = "Super Hard";
+                break;
+            default:
+                difficulty = "Unknown";
+                break;
+        }
     }
-  }
 
 });
--- js/results.js
+++ js/results.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
     message = "";
+    var questionCount = localStorage.getItem('questionCount');
+    var numCorrect = localStorage.getItem('numCorrect');
 
     // check to see if the user had already taken the test
     // if not, display an error; otherwise, continue with the quiz results
-    if (numAnswered == 0) {
+    if (questionCount == 0) {
         message = "ERROR: Please take the quiz first, silly! ";
         $('#resultmsg').html(message);
     } else {
@@ -13,6 +15,10 @@ $(document).ready(function() {
 
     function checkResults() {
         // display the results here
+        $('#questionscorrect').append(numCorrect);
+        $('#aquestionstotal').append(questionCount);
     }
+
+    localStorage.clear();
 
 })
